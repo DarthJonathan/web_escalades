38128af52f224fc19ff1e22b5cc5d9f2a2e203d5
Jonathan Hosea <jonathan.hosea@hotmail.com>
2014-08-12 09:36:11 +0700

    added code for take the last entry

--- live-score.php
+++ live-score.php
@@ -15,14 +15,14 @@
     	<?php
 			$con = mysql_connect("localhost", "root", "") or die ('error_user');
 			$db_selected = mysql_select_db("k9539147_esc",$con) or die ('error_db');
-			$sql = "SELECT * from basket";
+			$sql = "SELECT * FROM basket ORDER BY id DESC LIMIT 1";
 			$result = mysql_query($sql,$con) or die ('query_error');
 			$data=mysql_fetch_row($result);			
     	?>
         <h2 class="tagline" align="center">Basket</h2>
         <table>
         	<tr>
-            	<td><h3 class="tagline"><? $data[1]  ?></h2></td>
+            	<td><h3 class="tagline"><?  $data[1]  ?></h2></td>
                 <td><h1 class="tagline"><?  $data[3]  ?></h1></td>
                 <td><h1 class="tagline"><?  $data[4]  ?></h1></td>
                 <td><h3 class="tagline"><?  $data[2]  ?></h2></td>
