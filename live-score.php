ca341b6a7a057b41df537c2268b66217ef85ef95
Jonathan Hosea <jonathan.hosea@hotmail.com>
2014-08-12 09:40:46 +0700

    added connection for database

--- live-score.php
+++ live-score.php
@@ -27,6 +27,14 @@
                 <td><h1 class="tagline"><?  $data[4]  ?></h1></td>
                 <td><h3 class="tagline"><?  $data[2]  ?></h2></td>
             </tr>
+            <? mysql_close($con); ?>
+            <?php
+			$con = mysql_connect("localhost", "root", "") or die ('error_user');
+			$db_selected = mysql_select_db("k9539147_esc",$con) or die ('error_db');
+			$sql = "SELECT * FROM futsal ORDER BY id DESC LIMIT 1";
+			$result = mysql_query($sql,$con) or die ('query_error');
+			$data=mysql_fetch_row($result);			
+    		?>
             <tr>
             	<td><h3 class="tagline"><?  $data[1]  ?></h2></td>
                 <td><h1 class="tagline"><?  $data[3]  ?></h1></td>
