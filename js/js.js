5cf1a623936e8f70d6debccc65611676bb6b4f16
Jonathan Hosea <jonathan.hosea@me.com>
2014-08-13 06:01:19 -0700

    added carousel for "warung" missing images!

--- js/js.js
+++ js/js.js
@@ -43,4 +43,10 @@ $(document).ready(function ($) {
 		src:'https://www.youtube.com/watch?v=9wH5oWtbOFU'
 		}
 		});
+	
+	$("#warung").owlCarousel({
+    items : 4,
+    lazyLoad : true,
+    navigation : true
+  	}); 
 })
