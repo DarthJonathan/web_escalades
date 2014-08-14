08814d4d6fdb39a95cca054bb87af36042a7cb5d
Jonathan Hosea <jonathan.hosea@me.com>
2014-08-13 17:48:10 -0700

    added script

--- js/js.js
+++ js/js.js
@@ -22,17 +22,7 @@ $(function(){
 
 $(document).ready(function ($) {
 
-	$.stellar(
-	{
-	horizontalScrolling: false,
-    verticalScrolling: true,
-    scrollProperty: 'scroll',
-    positionProperty: 'transform',
-    parallaxBackgrounds: false,
-    parallaxElements: true,
-    responsive: true,
-    hideDistantElements: false
-	});
+	
 	
 	adjustScreen();
 	
@@ -50,3 +40,51 @@ $(document).ready(function ($) {
     navigation : true
   	}); 
 })
+
+//Disable Parallax On Mobile
+var stellarApplied = false;
+
+function removeParallax(vertOffset)
+{
+    if (stellarApplied)
+    {
+        stellarApplied = false;
+        $.stellar("destroy");
+        $("*[data-stellar-ratio]").attr("style", "");
+        $(".parallax[data-stellar-background-ratio]").css("background-position", "50% 50%");
+    }
+}
+
+function SetUpParallax (vertOffset)
+{
+    vertOffset = vertOffset ? vertOffset : 0;
+
+    // Don't enable parallax on touch devices (aka mobile/tablet)
+    if (!Modernizr.touch && window.screen.width > 768)
+    {
+    	if($(window).width() <= 768)
+    	{
+            removeParallax();
+        }
+        else
+        {
+        	if(!stellarApplied)
+        	{
+                $.stellar({
+                    
+                    //-- Set scrolling to be in either one or both directions
+                    horizontalScrolling: false,
+                    verticalScrolling: true,
+                    scrollProperty: 'scroll',
+                    positionProperty: 'transform',
+                    parallaxBackgrounds: false,
+                    parallaxElements: true,
+                    responsive: false,
+                    hideDistantElements: false
+                });
+
+                stellarApplied = true;
+            }
+        }
+    }
+}
