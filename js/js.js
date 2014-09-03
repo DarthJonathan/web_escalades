00e2435860b18841e76e9cda729c66832bab6f1d
Jonathan Hosea <jonathan.hosea@me.com>
2014-09-03 21:54:45 +0700

    refreshes build

--- js/js.js
+++ js/js.js
@@ -58,7 +58,95 @@ $(document).ready(function ($) {
 		src:'https://www.youtube.com/watch?v=9wH5oWtbOFU'
 		}
 		});
-	
+		
+	$('#path').magnificPopup({
+		items:
+		{
+		type:'inline',
+		src:'<div class="white-popup">Path Kami : Escalades Revinesia</div>'
+		}
+		});
+		
+	$('.band').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=10'
+		}
+		});
+		
+	$('.vocal').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=32'
+		}
+		});
+		
+	$('.PU').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=7'
+		}
+		});
+		
+	$('.basket').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=12'
+		}
+		});
+		
+	$('.catur').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=14'
+		}
+		});
+		
+	$('.futsal').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=21'
+		}
+		});
+		
+	$('.md').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=23'
+		}
+		});
+		
+	$('.mural').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=25'
+		}
+		});
+		
+	$('.paskibra').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=28'
+		}
+		});
+		
+	$('.story').magnificPopup({
+		items:
+		{
+		type:'iframe',
+		src:'http://www.osissmak5penaburjakarta.com/escalades/wp/?p=30'
+		}
+		});
+		
 	$("#warung").owlCarousel({
     items : 3,
     lazyLoad : true,
