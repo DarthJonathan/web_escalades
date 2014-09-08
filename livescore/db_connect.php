926cf15ff5226f609c0c20c169cf620e70134fd8
Jonathan Hosea <jonathan.hosea@hotmail.com>
2014-09-08 16:26:01 +0700

    Update db_connect.php

--- livescore/db_connect.php
+++ livescore/db_connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysql_connect("localhost", "root", "abcd1234") or die ('error_user');
+$con = mysql_connect("localhost", "k9539147_esc", "revitalisasi") or die ('error_user');
 $db_selected = mysql_select_db("k9539147_esc",$con) or die ('error_db');
 ?>
